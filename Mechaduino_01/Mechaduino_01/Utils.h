2a2d0a18485f85471a3c1eef626906117915eafb
jcchurch13 <jcchurch@mit.edu>
2016-11-26 22:14:53 -0500

    more improvements

--- Mechaduino_01/Mechaduino_01/Utils.h
+++ Mechaduino_01/Mechaduino_01/Utils.h
@@ -20,8 +20,6 @@
 
 	void parameterQuery();            //Prints current parameters
 	
-	float lookup_angle(int n);        //takes encoder reading and returns calibration-corrected angle
-	
 	void oneStep(void);               //take one step
 		
 	int readEncoder();                //read raw encoder position
