85b3d8c37f96cd76838f14ba4063cfbcb3e62e09
jcchurch13 <jcchurch@mit.edu>
2017-06-04 13:47:02 -0400

    fixed stepangle typo

--- Mechaduino/Mechaduino/Mechaduino.ino
+++ Mechaduino/Mechaduino/Mechaduino.ino
@@ -87,7 +87,7 @@ void loop()                 // main loop
 
   serialCheck();              //must have this execute in loop for serial commands to function
 
-  //r=0.1125*step_count;      //Don't use this anymore. Step interrupts enabled above by "configureStepDir()", adjust step size in parameters.cpp
+  //r=0.1125*step_count;      //Don't use this anymore. Step interrupts enabled above by "configureStepDir()", adjust step size ("stepangle")in parameters.cpp
 
 }
 
